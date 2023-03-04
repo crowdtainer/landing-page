130da3feb3762bc1e6d6a0264b60b1710480bf11
Thiago Faria Alencar <thiago@ti.eng.br>
2023-03-04 21:21:01 +0100

    Enable trailingSlash to work well with Caddy

--- src/routes/Screenshots/+page.svelte
+++ src/routes/Screenshots/+page.svelte
@@ -3,27 +3,27 @@
 		<p class="text-xl">Example project:</p>
 	</div>
 	<div class="flex justify-center my-5 pb-20 space-x-4 w-full">
-		<img alt="Project screenshot" src="images/ResponsiveUI.png" />
+		<img alt="Project screenshot" src="/images/ResponsiveUI.png" />
 	</div>
 
 	<div class="flex justify-center">
 		<p class="text-xl mx-10">Users can leave before before funding time completes:</p>
 	</div>
 	<div class="flex justify-center my-5 pb-20 space-x-4 w-full">
-		<img alt="Project screenshot" src="images/screen_Leave.png" />
+		<img alt="Project screenshot" src="/images/screen_Leave.png" />
 	</div>
 
 	<div class="flex justify-center">
 		<p class="text-xl mx-10">User joined a successfull project:</p>
 	</div>
 	<div class="flex justify-center my-5 pb-20 space-x-4">
-		<img alt="Project screenshot" src="images/screen_Success.png" />
+		<img alt="Project screenshot" src="/images/screen_Success.png" />
 	</div>
 
 	<div class="flex justify-center">
 		<p class="text-xl mx-10">Check-out page:</p>
 	</div>
 	<div class="flex justify-center my-5 pb-20 space-x-4">
-		<img alt="Project screenshot" src="images/screen_Checkout.png" />
+		<img alt="Project screenshot" src="/images/screen_Checkout.png" />
 	</div>
 </div>
